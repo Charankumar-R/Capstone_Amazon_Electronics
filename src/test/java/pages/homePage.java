2d2ee69aa589a89d6984eebec2d00e07f5046331
kailashkarthikeyan <kailashkarthikeyanms@gmail.com>
2026-02-13 12:06:26 +0530

    electronic

--- src/test/java/pages/homePage.java
+++ src/test/java/pages/homePage.java
@@ -1,5 +1,15 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+
 public class homePage {
+
+    @FindBy(css="//a[normalize-space()='Electronics']"
+            public WebElement electronic;
+    electronic.click();
+
 
 }
