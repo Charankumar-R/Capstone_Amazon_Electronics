887fb14257939c070013609c46e3d37d2c4b6175
Charankumar-R <117891438+Charankumar-R@users.noreply.github.com>
2026-02-13 12:09:30 +0530

    Test Case

--- src/test/java/test/catogeries.java
+++ src/test/java/test/catogeries.java
@@ -1,10 +1,12 @@
 package test;
 
 import org.testng.annotations.Test;
+import pages.homePage;
 
 public class catogeries {
     @Test
     public void catogeriesTest() {
+        homePage homePage = new homePage();
 
     }
 }
